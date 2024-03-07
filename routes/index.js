ff9efdf83d4bd6e745c632adacf877d5d8dcbc51
watsawee <watsawee@gmail.com>
2024-03-07 11:49:27 +0700

    add word count feature to the function

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
  
-function file_readline(){
+function file_readline(fav_team){
+  let cnt = 0;
   const contents = fs.readFileSync('public\\input.txt', 'utf-8');
   contents.split(/\r?\n/).forEach(line =>  {
     console.log(`Line from file: ${line}`);
+    if(line == fav_team) cnt++;
   });
+  return cnt;
 }
 
 /* GET home page. */
@@ -16,7 +19,10 @@ router.get('/', function(req, res, next) {
 
 router.get('/wordcnt/:fav_team', function(req, res) {
   let fav_team = req.params.fav_team;
-  file_readline();
+  //----------------
+  let cnt = file_readline(fav_team);
+  console.log("word count -> "+cnt);
+  //----------------
   res.send('word count API - '+fav_team);
 });
 
